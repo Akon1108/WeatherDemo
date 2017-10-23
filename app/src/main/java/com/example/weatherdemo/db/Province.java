aee18c77305fe9e42e31cc37b861c3e6de3c60e5
Wangdk <wangdk.akon@gmail.com>
2017-10-23 13:59:23 +0800

    First commit.

---
+++ app/src/main/java/com/example/weatherdemo/db/Province.java
@@ -0,0 +1,34 @@
+package com.example.weatherdemo.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Wangdk on 2017/10/11.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId(){
+        return id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName = provinceName;
+    }
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
+
+}
